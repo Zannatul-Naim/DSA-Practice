94639f3c3d1ec4e5cbf26c74c42f12b392c2db68
Zannatul Naim <zannatulnaim09@gmail.com>
2023-01-09 20:48:41 +0600

    Update tower_of_hanoi.cpp

--- Recursion/tower_of_hanoi.cpp
+++ Recursion/tower_of_hanoi.cpp
@@ -1,25 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void tower_of_hanoi(int n, char sour, char aux, char dest)
-{
+void tower_of_hanoi(int n, char src, char aux, char dest) {
     if(n == 1)
     {
-        cout<<"move disk "<<n<<" from "<<sour<<" to "<<dest<<endl;
-    }
-    else{
-    tower_of_hanoi(n-1, sour, dest, aux);
-    cout<<"move disk "<<n<<" from "<<sour<<" to "<<dest<<endl; 
-    tower_of_hanoi(n-1, aux, sour, dest);
+        cout << "move disk " << n << " from " << src << " to " << dest << endl;
+    } else {  // recursive call
+        tower_of_hanoi(n-1, src, dest, aux);
+        cout << "move disk " << n << " from " << src << " to " << dest << endl; 
+        tower_of_hanoi(n-1, aux, src, dest);
     }
 }
 
 int main()
 {
-
     int n;
-    cout<<"Enter the number of disks: "<<endl;
-    cin>>n;
+    cout << "Enter the number of disks: " << endl;
+    cin >> n;
     tower_of_hanoi(n, 'A', 'B', 'C');
     return 0;
 }
